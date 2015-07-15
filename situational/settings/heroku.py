b2e8f3f4d4a0bf579fcb4ee4b258595804a86e7d
Sym Roe <sym.roe@talusdesign.co.uk>
2015-07-15 17:51:06 +0100

    Add heroku settings

---
+++ situational/settings/heroku.py
@@ -0,0 +1,16 @@
+from .base import *
+
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    ('Sym Roe', 'lmtools@talusdesign.co.uk'),
+)
+
+MANAGERS = ADMINS
+
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
