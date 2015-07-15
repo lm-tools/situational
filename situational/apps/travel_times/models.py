6e14e7221678a7714f71958161712b82d3573460
Gareth Visagie <gareth@gjvis.com>
2015-07-15 16:45:44 +0100

    Extract population of travel times map

--- situational/apps/travel_times/models.py
+++ situational/apps/travel_times/models.py
@@ -1,5 +1,3 @@
-import uuid
-
 from django.core.files import File
 from django.conf import settings
 from django.db import models
@@ -33,13 +31,6 @@ class TravelTimesMap(models.Model):
 
 
 class TravelTimesMapRepository(object):
-    def __init__(self, client=None):
-        if not client:
-            client = getattr(settings, 'MAPUMENTAL_CLIENT', mapumental.Client)
-        self.client = client()
-        self.depart_at = '0800'
-        self.arrive_before = '0930'
-
     def get(self, postcode, width, height):
         travel_times_map, _created = TravelTimesMap.objects.get_or_create(
             postcode=postcode,
@@ -48,23 +39,35 @@ class TravelTimesMapRepository(object):
             )
 
         if not travel_times_map.image:
-            print(
-                "Fetching travel time map for %s, w=%s, h=%s" %
-                (postcode, width, height)
-                )
-            image = self.client.get(
-                postcode,
-                width,
-                height,
-                self.depart_at,
-                self.arrive_before,
-                )
-            travel_times_map.mime_type = image.mime_type
-            travel_times_map.image.save(
-                str(uuid.uuid4()),
-                File(image.file),
-                False,
-                )
-            travel_times_map.save()
+            populator = TravelTimesMapPopulator()
+            travel_times_map = populator.populate(travel_times_map)
 
         return travel_times_map
+
+
+class TravelTimesMapPopulator(object):
+    def __init__(self, client=None):
+        if not client:
+            client = getattr(settings, 'MAPUMENTAL_CLIENT', mapumental.Client)
+        self.client = client()
+        self.depart_at = '0800'
+        self.arrive_before = '0930'
+
+    def populate(self, map):
+        image = self.client.get(
+            map.postcode,
+            map.width,
+            map.height,
+            self.depart_at,
+            self.arrive_before,
+            )
+
+        map.mime_type = image.mime_type
+        map.image.save(
+            "%s-w%s-h%s" % (map.postcode, map.width, map.height),
+            File(image.file),
+            False,
+            )
+        map.save()
+
+        return map
