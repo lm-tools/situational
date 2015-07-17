93a5d6f71068a8da95a37329f9bfec2ee3f27233
Gareth Visagie <gareth@gjvis.com>
2015-07-17 11:56:35 +0100

    Store files on S3

--- situational/settings/heroku.py
+++ situational/settings/heroku.py
@@ -1,5 +1,6 @@
-from .base import *
+import os
 
+from .base import *
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -15,3 +16,9 @@ import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_S3_HOST = os.environ['AWS_S3_HOST']
