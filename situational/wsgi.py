a98c13674ae9d68d956f7ee00562f85b032bb5e0
Gareth Visagie <gareth@gjvis.com>
2015-07-17 12:26:09 +0100

    Import whitenoise after default settings

--- situational/wsgi.py
+++ situational/wsgi.py
@@ -17,14 +17,13 @@ import os
 from os.path import abspath, dirname
 from sys import path
 
-from whitenoise.django import DjangoWhiteNoise
-
 SITE_ROOT = dirname(dirname(abspath(__file__)))
 path.append(SITE_ROOT)
 
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "situational.settings")
 
+from whitenoise.django import DjangoWhiteNoise
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
