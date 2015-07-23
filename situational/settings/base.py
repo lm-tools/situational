5cf96fa57571ce701bdb060559067c6747228993
Gareth Visagie <gareth@gjvis.com>
2015-07-23 11:09:45 +0100

    Allow JOBS_API_BASE_URL to be provided by the env

--- situational/settings/base.py
+++ situational/settings/base.py
@@ -176,7 +176,7 @@ LOGGING = {
 # EMAILS
 
 # Jobs API
-JOBS_API_BASE_URL = "https://lm-tools-jobs-api.herokuapp.com"
+JOBS_API_BASE_URL = environ.get('JOBS_API_BASE_URL', 'https://lmt-jobs-api.herokuapp.com')
 
 # MAPUMENTAL
 MAPUMENTAL_API_KEY = environ.get('MAPUMENTAL_API_KEY')
