d308a445c36e491622630dbcc562238888111f12
Gareth Visagie <gareth@gjvis.com>
2015-07-30 15:53:23 +0100

    Refactor report model tests

--- situational/apps/report/tests/test_models.py
+++ situational/apps/report/tests/test_models.py
@@ -18,66 +18,49 @@ class TestReportModel(TestCase):
 
 
 class TestReportIsPopulated(TestCase):
-    def _populated_report(self):
-        dummy_travel_times_map = TravelTimesMap(
+    def _dummy_travel_times_map(self):
+        travel_times_map, created = TravelTimesMap.objects.get_or_create(
             postcode='SW1A 1AA',
             height=1200,
-            width=1200
+            width=1200,
         )
-        dummy_travel_times_map.image.save(
-            'dummy_file.gif',
-            File(BytesIO(
-                b64decode("R0lGODlhAQABAAAAACH5BAEAAAAALAAAAAABAAEAAAI=")
-            )),
-            False,
-        )
-        dummy_travel_times_map.save()
+        if not travel_times_map.has_image:
+            travel_times_map.image.save(
+                'dummy_file.gif',
+                File(BytesIO(
+                    b64decode("R0lGODlhAQABAAAAACH5BAEAAAAALAAAAAABAAEAAAI=")
+                ))
+            )
+        return travel_times_map
 
-        return Report(
-            postcode='SW1A 1AA',
-            location_json='{"wgs84_lon": -0.141, "wgs84_lat": 51.501}',
-            top_categories='[{"category": "Testing Jobs", "count": 165}]',
-            top_companies='[{"company_name": "The Test Group"}]',
-            latest_jobs='[{"company_name": "The Test Kitchen"}]',
-            travel_times_map=dummy_travel_times_map,
-        )
-
-    def _populated_report_without(self, field, blank_value=''):
-        report = self._populated_report()
-        setattr(report, field, blank_value)
-        return report
+    def _populated_report(self, without=[]):
+        populated_fields = {
+            'postcode': 'SW1A 1AA',
+            'location_json': '{"wgs84_lon": -0.141, "wgs84_lat": 51.501}',
+            'top_categories': '[{"category": "Testing Jobs", "count": 165}]',
+            'top_companies': '[{"company_name": "The Test Group"}]',
+            'latest_jobs': '[{"company_name": "The Test Kitchen"}]',
+            'travel_times_map': self._dummy_travel_times_map(),
+        }
+        for field in without:
+            del populated_fields[field]
+        return Report(**populated_fields)
 
     def test_new_reports_are_considered_unpopulated(self):
         self.assertFalse(Report().is_populated)
 
-    def test_reports_with_all_contents_present_are_considered_populated(self):
+    def test_reports_with_all_fields_present_are_considered_populated(self):
         self.assertTrue(self._populated_report().is_populated)
 
-    def test_reports_without_location_json_are_considered_unpopulated(self):
-        self.assertFalse(
-            self._populated_report_without('location_json').is_populated
-        )
-
-    def test_reports_without_top_categories_are_considered_unpopulated(self):
-        self.assertFalse(
-            self._populated_report_without('top_categories').is_populated
-        )
-
-    def test_reports_without_top_companies_are_considered_unpopulated(self):
-        self.assertFalse(
-            self._populated_report_without('top_companies').is_populated
-        )
-
-    def test_reports_without_latest_jobs_are_considered_unpopulated(self):
-        self.assertFalse(
-            self._populated_report_without('latest_jobs').is_populated
-        )
-
-    def test_reports_without_travel_times_map_are_considered_unpopulated(self):
-        self.assertFalse(
-            self._populated_report_without('travel_times_map', None)
-                .is_populated
-        )
+    def test_reports_with_missing_fields_are_considered_unpopulated(self):
+        for field in ('location_json',
+                      'top_categories',
+                      'top_companies',
+                      'latest_jobs',
+                      'travel_times_map'):
+            with self.subTest(field=field):
+                report = self._populated_report(without=[field])
+                self.assertFalse(report.is_populated)
 
     def test_reports_without_travel_images_are_considered_unpopulated(self):
         report = self._populated_report()
