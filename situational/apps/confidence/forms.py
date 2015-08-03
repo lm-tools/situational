5c5595edbc305de239ea3cdf55ded03a1ad90e94
Gareth Visagie <gareth@gjvis.com>
2015-08-03 17:49:40 +0100

    Capture and display confidence report details

--- situational/apps/confidence/forms.py
+++ situational/apps/confidence/forms.py
@@ -2,4 +2,31 @@ from django import forms
 
 
 class ConfidenceReportForm(forms.Form):
-    pass
+    success_confidence = forms.ChoiceField(
+        label="How confident are you that you will find a work?",
+        choices=(
+            (1, "Not at all - I have serious barriers to overcome"),
+            (2, ""),
+            (3, ""),
+            (4, "About average"),
+            (5, ""),
+            (6, ""),
+            (7, "Very confident - I will find the right thing, quickly"),
+        ),
+        widget=forms.RadioSelect(),
+    )
+
+    search_confidence = forms.ChoiceField(
+        label="How confident are you searching for work?",
+        choices=(
+            (1, "Not at all - I don't know where to start"),
+            (2, ""),
+            (3, ""),
+            (4, "About average"),
+            (5, ""),
+            (6, ""),
+            (7, "Very confident - I can search online, write cover letters, "
+                "meet people face-to-face etc"),
+        ),
+        widget=forms.RadioSelect(),
+    )
--- situational/apps/confidence/views.py
+++ situational/apps/confidence/views.py
@@ -11,3 +11,10 @@ class NewConfidenceReport(FormView):
 
 class ConfidenceReport(TemplateView):
     template_name = 'confidence/show.html'
+
+    def get_context_data(self, **kwargs):
+        context = kwargs
+        q = self.request.GET
+        context['success_confidence'] = q.get('success_confidence')
+        context['search_confidence'] = q.get('search_confidence')
+        return context
