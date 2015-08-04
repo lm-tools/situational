f8195f688fc3cd1eab64454a2151249077b5c3d4
Sym Roe <sym.roe@talusdesign.co.uk>
2015-08-04 14:00:59 +0100

    Text and style updates

--- situational/apps/sectors/forms.py
+++ situational/apps/sectors/forms.py
@@ -5,6 +5,12 @@ from django import forms
 from localflavor.gb.forms import GBPostcodeField
 
 
+class NoColonForm(forms.Form):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault('label_suffix', '')
+        super().__init__(*args, **kwargs)
+
+
 class SectorSelectorWidget(forms.MultiWidget):
     def __init__(self, count, attrs=None):
         _widgets = []
@@ -19,8 +25,11 @@ class SectorSelectorWidget(forms.MultiWidget):
         return []
 
 
-class SectorForm(forms.Form):
+class SectorForm(NoColonForm):
     SECTOR_INPUT_COUNT = 3
 
     postcode = GBPostcodeField()
-    sector = forms.CharField(widget=SectorSelectorWidget(SECTOR_INPUT_COUNT))
+    sector = forms.CharField(
+        widget=SectorSelectorWidget(SECTOR_INPUT_COUNT),
+        label="Job roles eg security, data entry, driver",
+    )
