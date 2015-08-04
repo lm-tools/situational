06d258bd65c4713586535b09eac5806c57227c60
Gareth Visagie <gareth@gjvis.com>
2015-08-04 11:56:46 +0100

    Add postcode db index to reports

---
+++ situational/apps/report/migrations/0005_auto_20150804_1154.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('report', '0004_remove_report_place_name'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='report',
+            name='postcode',
+            field=models.CharField(max_length=14, db_index=True),
+        ),
+    ]
