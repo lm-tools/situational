54ac0feab37643bc1a78ae1f15c5de2b606a93f3
Camille Baldock <camille@camillebaldock.co.uk>
2015-08-12 12:12:11 +0100

    Hardening: validation, improve copy

--- situational/apps/history/forms.py
+++ situational/apps/history/forms.py
@@ -2,20 +2,17 @@ from django import forms
 
 
 class HistoryDetailsForm(forms.Form):
-    circumstances = forms.MultipleChoiceField(
-        widget=forms.CheckboxSelectMultiple,
-        choices=[
-            ("full_time", "Full time"),
-            ("part_time", "Part time"),
-            ("work_programme", "Work programme"),
-            ("sick", "Sick"),
-            ("caring", "Caring"),
-            ("children", "Children"),
-            ("education", "Education"),
-            ("unemployed", "Unemployed"),
-            ("other", "Other"),
-        ]
-    )
+    CIRCUMSTANCE_CHOICES = [
+        ("full_time", "Full time"),
+        ("part_time", "Part time"),
+        ("work_programme", "Work programme"),
+        ("sick", "Sick"),
+        ("caring", "Caring"),
+        ("children", "Children"),
+        ("education", "Education"),
+        ("unemployed", "Unemployed"),
+        ("other", "Other"),
+    ]
     DURATION_CHOICES = [
         ('less_than_1', 'less than 1 month'),
         ('1_to_3', '1 to 3 months'),
@@ -26,8 +23,24 @@ class HistoryDetailsForm(forms.Form):
         ('18_to_24', '18 to 24 months'),
         ('over_24', 'over 24 months')
     ]
+    circumstances = forms.MultipleChoiceField(
+        widget=forms.CheckboxSelectMultiple,
+        choices=CIRCUMSTANCE_CHOICES
+    )
     duration = forms.ChoiceField(
         choices=DURATION_CHOICES,
         widget=forms.RadioSelect())
     other_more = forms.CharField(required=False)
     description = forms.CharField(required=False)
+
+    def clean(self):
+        cleaned_data = super(HistoryDetailsForm, self).clean()
+        circumstances = cleaned_data.get("circumstances")
+        other_more = cleaned_data.get("other_more")
+        if circumstances and "other" in circumstances and not other_more:
+            description_missing = "Please enter a short description for other"
+            self.add_error('circumstances', description_missing)
+        if circumstances and "other" not in circumstances and other_more:
+            select_other = "Please select 'other' if you enter a description"
+            self.add_error('circumstances', select_other)
+        return cleaned_data
--- situational/apps/history/views.py
+++ situational/apps/history/views.py
@@ -7,6 +7,33 @@ from django.views.generic import View
 from history import forms
 
 
+def get_form_data_from_session(session):
+    form_data = session['forms']
+    for form in form_data:
+        form.pop("csrfmiddlewaretoken", None)
+    return form_data
+
+
+def format_history_entry(entry):
+    description = ""
+    if entry["description"][0]:
+        description = "({0})".format(entry["description"][0])
+    circumstance_data = entry["circumstances"]
+    if entry["other_more"][0]:
+        circumstance_data += entry["other_more"]
+        circumstance_data.remove('other')
+    formatted_circumstances = list(map(format_circumstance, circumstance_data))
+    circumstances = ", ".join(formatted_circumstances)
+    duration_dict = dict(forms.HistoryDetailsForm.DURATION_CHOICES)
+    duration = duration_dict[entry["duration"][0]]
+    return "For {0}: {1} {2}".format(duration, circumstances, description)
+
+
+def format_circumstance(circumstance):
+    circumstance_dict = dict(forms.HistoryDetailsForm.CIRCUMSTANCE_CHOICES)
+    return circumstance_dict.get(circumstance, circumstance)
+
+
 class HistoryDetailsView(FormView):
     """
     Render HistoryDetailsView and redirect to HistoryReportView when
@@ -16,10 +43,18 @@ class HistoryDetailsView(FormView):
     template_name = "history/details.html"
     form_class = forms.HistoryDetailsForm
 
-    def form_valid(self, form):
+    def get(self, request, *args, **kwargs):
         if 'forms' not in self.request.session:
             self.request.session['forms'] = []
-        self.request.session['forms'] += [form.data]
+        if len(self.request.session['forms']) >= 3:
+            url = reverse('history:report')
+            return http.HttpResponseRedirect(url)
+        else:
+            response = super().get(request, *args, **kwargs)
+            return response
+
+    def form_valid(self, form):
+        self.request.session['forms'] += [dict(form.data.lists())]
         if len(self.request.session['forms']) < 3:
             url = reverse('history:details')
         else:
@@ -28,29 +63,28 @@ class HistoryDetailsView(FormView):
 
     def get_context_data(self, **kwargs):
         context = kwargs
-
-        def remove_csrf_field(form_data):
-            form_data.pop("csrfmiddlewaretoken", None)
-            return form_data
-        form_data = [
-            remove_csrf_field(l) for l in self.request.session['forms']]
-        context['report'] = form_data
+        history_data = get_form_data_from_session(self.request.session)
+        if history_data:
+            context['history'] = map(format_history_entry, history_data)
+            context['circumstance_title'] = "Your circumstances previously"
+        else:
+            context['circumstance_title'] = "Your current circumstances"
         return context
 
 
 class HistoryReportView(TemplateView):
     def get(self, request, *args, **kwargs):
-        self.template_name = "history/report.html"
-        response = super().get(request, *args, **kwargs)
-        return response
+        session = self.request.session
+        if 'forms' not in session or len(session['forms']) < 3:
+            url = reverse('history:details')
+            return http.HttpResponseRedirect(url)
+        else:
+            self.template_name = "history/report.html"
+            response = super().get(request, *args, **kwargs)
+            return response
 
     def get_context_data(self, **kwargs):
         context = kwargs
-
-        def remove_csrf_field(form_data):
-            form_data.pop("csrfmiddlewaretoken", None)
-            return form_data
-        form_data = [
-            remove_csrf_field(l) for l in self.request.session['forms']]
-        context['report'] = form_data
+        history_data = get_form_data_from_session(self.request.session)
+        context['report'] = map(format_history_entry, history_data)
         return context
