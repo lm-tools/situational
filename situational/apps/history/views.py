dfc767e81d1cf13d3d08ff4d27e7f0431e365bb8
Camille Baldock <camille@camillebaldock.co.uk>
2015-08-21 14:14:12 +0100

    Remove quick history pages from history

--- situational/apps/history/views.py
+++ situational/apps/history/views.py
@@ -5,14 +5,7 @@ from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 from django.views.generic import FormView
 from django.views.generic import View
-from history import forms
-
-
-def get_form_data_from_session(session):
-    form_data = session.get('forms', [])
-    for form in form_data:
-        form.pop("csrfmiddlewaretoken", None)
-    return form_data
+from . import forms
 
 
 def format_summary(session):
@@ -95,51 +88,6 @@ def remove_csrf_token(data):
     return data
 
 
-def format_timeline_data(history_data):
-    nb_months = total_number_of_months(history_data)
-    result = {}
-    result["years"] = timeline_years_dict(nb_months)
-    result["timeline"] = circumstance_timeline(history_data, nb_months)
-    return result
-
-
-def total_number_of_months(history_data):
-    result = 0
-    for entry in history_data:
-        result += length_in_months(entry)
-    return result
-
-
-def circumstance_timeline(history_data, nb_months):
-    result = {}
-    unique_circumstances = []
-    circumstances = []
-    for entry in history_data:
-        circumstances += format_circumstances(entry)
-    unique_circumstances = unique_items_from_list(circumstances)
-    for unique_circumstance in unique_circumstances:
-        result[unique_circumstance] = []
-        for entry in history_data:
-            duration = length_in_months(entry)
-            active = unique_circumstance in format_circumstances(entry)
-            result[unique_circumstance] += [
-                {"length": duration / nb_months * 100, "active": active}
-            ]
-    return result
-
-
-def length_in_months(entry):
-    duration_length_dict = dict(forms.HistoryDetailsForm.DURATION_LENGTHS)
-    duration = duration_length_dict[entry["duration"][0]]
-    return duration
-
-
-def unique_items_from_list(list_with_dups):
-    result = []
-    [result.append(i) for i in list_with_dups if i not in result]
-    return result
-
-
 def timeline_years_dict(nb_of_months):
     years = []
     now = datetime.datetime.now()
@@ -180,93 +128,9 @@ def get_employment_context(session):
         return False
 
 
-def history_entry_as_string(entry):
-    description = ""
-    if entry["description"][0]:
-        description = "({0})".format(entry["description"][0])
-    formatted_circumstances = format_circumstances(entry)
-    circumstances = ", ".join(formatted_circumstances)
-    duration_dict = dict(forms.HistoryDetailsForm.DURATION_CHOICES)
-    duration = duration_dict[entry["duration"][0]]
-    return "For {0}: {1} {2}".format(duration, circumstances, description)
-
-
-class HistoryDetailsView(FormView):
-    """
-    Render HistoryDetailsView and redirect to HistoryReportView when
-    the form has been completed 3 times
-    """
-
-    template_name = "history/details.html"
-    form_class = forms.HistoryDetailsForm
-
+class StartView(TemplateView):
     def get(self, request, *args, **kwargs):
-        if len(self.request.session.get('forms', [])) >= 3:
-            url = reverse('history:report')
-            return http.HttpResponseRedirect(url)
-        else:
-            response = super().get(request, *args, **kwargs)
-            return response
-
-    def form_valid(self, form):
-        if 'forms' not in self.request.session:
-            self.request.session['forms'] = []
-        self.request.session['forms'] += [dict(form.data.lists())]
-        if len(self.request.session['forms']) < 3:
-            url = reverse('history:details')
-        else:
-            url = reverse('history:report')
-        return http.HttpResponseRedirect(url)
-
-    def get_context_data(self, **kwargs):
-        context = kwargs
-        history_data = get_form_data_from_session(self.request.session)
-        if history_data:
-            context['history'] = map(history_entry_as_string, history_data)
-            context['circumstance_title'] = "Your circumstances previously"
-            context['percentage'] = len(history_data) * 100 / 3
-        else:
-            context['circumstance_title'] = "Your current circumstances"
-            context['percentage'] = 0
-        return context
-
-
-class HistoryReportView(TemplateView):
-    def get(self, request, *args, **kwargs):
-        session = self.request.session
-        if 'forms' not in session or len(session['forms']) < 3:
-            url = reverse('history:details')
-            return http.HttpResponseRedirect(url)
-        else:
-            self.template_name = "history/report.html"
-            response = super().get(request, *args, **kwargs)
-            return response
-
-    def get_context_data(self, **kwargs):
-        context = kwargs
-        history_data = get_form_data_from_session(self.request.session)
-        context['report'] = map(history_entry_as_string, history_data)
-        context['timeline'] = format_timeline_data(history_data)
-        return context
-
-
-class ClearSessionView(TemplateView):
-    def post(self, request, *args, **kwargs):
-        self.request.session['forms'] = []
-        url = reverse('history:details')
-        return http.HttpResponseRedirect(url)
-
-
-class HistoryStartView(TemplateView):
-    def get(self, request, *args, **kwargs):
-        self.template_name = "history/start_text.html"
-        response = super().get(request, *args, **kwargs)
-        return response
-
-
-class HistoryStartStructuredView(TemplateView):
-    def get(self, request, *args, **kwargs):
-        self.template_name = "history/start_structured.html"
+        self.template_name = "history/start.html"
         response = super().get(request, *args, **kwargs)
         return response
 
