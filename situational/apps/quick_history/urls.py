4848915eeaf68f534b369a0d43005455dd999038
Camille Baldock <camille@camillebaldock.co.uk>
2015-08-25 15:31:25 +0100

    No 'start' in URLs for start pages

--- situational/apps/quick_history/urls.py
+++ situational/apps/quick_history/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'report\.pdf', views.PDFView.as_view(), name="pdf"),
     url(r'clear_session', views.ClearSessionView.as_view(),
         name="clear_session"),
-    url(r'start$', views.StartView.as_view(),
+    url(r'^$', views.StartView.as_view(),
         name="start"),
     url(r'send$', views.SendView.as_view(),
         name="send"),
