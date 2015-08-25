52828342e452db56525d11556e2ab991fbcfeaf0
Gareth Visagie <gareth@gjvis.com>
2015-08-25 15:52:08 +0100

    Prevent "starts with" route matches

--- situational/apps/quick_history/urls.py
+++ situational/apps/quick_history/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
     url(r'details$', views.HistoryDetailsView.as_view(), name="details"),
     url(r'report$', views.HistoryReportView.as_view(), name="report"),
-    url(r'report\.pdf', views.PDFView.as_view(), name="pdf"),
-    url(r'clear_session', views.ClearSessionView.as_view(),
+    url(r'report\.pdf$', views.PDFView.as_view(), name="pdf"),
+    url(r'clear_session$', views.ClearSessionView.as_view(),
         name="clear_session"),
     url(r'^$', views.StartView.as_view(),
         name="start"),
