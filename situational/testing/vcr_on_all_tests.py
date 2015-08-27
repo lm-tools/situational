29dcfe72a4e31820519fa5e969ace4d414ce2d73
Gareth Visagie <gareth@gjvis.com>
2015-08-27 14:03:14 +0100

    Move VCR filter config

--- situational/testing/vcr_on_all_tests.py
+++ situational/testing/vcr_on_all_tests.py
@@ -13,10 +13,10 @@ def _cassette_path(function):
     cassette_path = module_path_part + function_path_part
     return os.path.join(_CASSETTE_LIBRARY_PATH, *cassette_path)
 
-
 _VCR = vcr.VCR(
     path_transformer=vcr.VCR.ensure_suffix('.yaml'),
-    func_path_generator=_cassette_path
+    func_path_generator=_cassette_path,
+    filter_query_parameters=['app_key', 'app_id'],
 )
 
 
@@ -25,13 +25,7 @@ class _VCROnAllTestsMeta(type):
         for attr_name, attr_value in iter(attrs.items()):
             if attr_name.startswith('test_') and \
                     isinstance(attr_value, types.FunctionType):
-                filter_args = [
-                    'app_key',
-                    'app_id',
-                ]
-                attrs[attr_name] = _VCR.use_cassette(
-                    attr_value,
-                    filter_query_parameters=filter_args)
+                attrs[attr_name] = _VCR.use_cassette(attr_value)
 
         return super(_VCROnAllTestsMeta, cls).__new__(cls, name, bases, attrs)
 
