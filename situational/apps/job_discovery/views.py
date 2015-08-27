150e96c7efdda0efedec84dfce1f7b402d0b15b4
Camille Baldock <camille@camillebaldock.co.uk>
2015-08-27 17:24:34 +0100

    Delete job cards view

--- situational/apps/job_discovery/views.py
+++ situational/apps/job_discovery/views.py
@@ -67,22 +67,3 @@ class ReportView(TemplateView):
         # get that report from the guid
         context["report"] = {}
         return context
-
-
-class JobCardsView(TemplateView):
-    template_name = "job_discovery/job_cards.html"
-
-    def get(self, request, *args, **kwargs):
-        response = super().get(request, *args, **kwargs)
-        return response
-
-    def get_context_data(self, **kwargs):
-        context = kwargs
-        az = Adzuna()
-        context['jobs'] = az.jobs_at_location(
-            'UK',
-            'London',
-            'Central London',
-            50
-        )
-        return context
