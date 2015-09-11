2a600e6e0beb13ed91302c7c94e34b7df665a9c0
Gareth Visagie <gareth@gjvis.com>
2015-09-11 15:41:32 +0100

    Update app names in manifests

--- situational/apps/sectors/apps.py
+++ situational/apps/sectors/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class SectorsConfig(AppConfig):
     name = 'sectors'
-    verbose_name = "Understand what sort of jobs you could do"
+    verbose_name = "Job roles"
     icon_url = 'images/icons/app-icon-red.png'
     start_url_name = "start"
     manifest = True
--- situational/apps/travel_report/apps.py
+++ situational/apps/travel_report/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class TravelReportConfig(AppConfig):
     name = 'travel_report'
-    verbose_name = "Understand where you could travel for work"
+    verbose_name = "Travel"
     icon_url = 'images/icons/app-icon-yellow.png'
     start_url_name = "start"
     manifest = True
