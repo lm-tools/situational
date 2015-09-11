e3d8a43ff024d3946feec1603d171c8f3561ecc4
Gareth Visagie <gareth@gjvis.com>
2015-09-11 15:58:52 +0100

    Make history app names fit on one android line

--- situational/apps/detailed_history/apps.py
+++ situational/apps/detailed_history/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class DetailedHistoryConfig(AppConfig):
     name = 'detailed_history'
-    verbose_name = "History (detailed)"
+    verbose_name = "History detailed"
     icon_url = 'images/icons/app-icon-fuschia.png'
     start_url_name = "start"
     manifest = True
