f6b153404c40b56dd1cab2ab48c861979199bbad
Gareth Visagie <gareth@gjvis.com>
2015-09-23 10:25:16 +0100

    Test feedback sending

---
+++ situational/apps/home_page/tests/test_views.py
@@ -0,0 +1,34 @@
+from django.core import mail
+from django.core.urlresolvers import reverse
+
+from situational.testing import BaseCase
+
+
+class TestHomePageView(BaseCase):
+    def test_post_sends_feedback(self):
+        response = self.client.post(
+            reverse('home_page:home'),
+            data={
+                'name': 'Harry Potter',
+                'email': 'test@example.org',
+                'subject': 'Home page design',
+                'message': 'Your home page needs more animated gifs',
+            }
+        )
+
+        with self.subTest('redirects to the thank you page'):
+            self.assertRedirects(response, reverse('home_page:thank_you'))
+
+        with self.subTest('sends the feedback email'):
+            self.assertEqual(len(mail.outbox), 1, 'Mail should have been sent')
+            message = mail.outbox[0]
+            self.assertIn('feedback@lm-tools.com', message.to)
+            self.assertIn('Home page design', message.subject)
+            self.assertIn('Harry Potter', message.body)
+            self.assertIn('test@example.org', message.body)
+            self.assertIn('Your home page needs more animated gifs',
+                          message.body)
+            self.assertIn('Harry Potter', message.alternatives[0][0])
+            self.assertIn('test@example.org', message.alternatives[0][0])
+            self.assertIn('Your home page needs more animated gifs',
+                          message.alternatives[0][0])
