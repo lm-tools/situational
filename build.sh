4a2940252fd505dd417d3bde2ac3f0708568842b
Gareth Visagie <gareth@gjvis.com>
2015-09-24 17:04:33 +0100

    Replace django-pipeline with django-static-precompiler
    Doesn't support JS minification out of the box, and we don't need it
    right now, so also removes the NodeJS dependency

--- build.sh
+++ build.sh
@@ -7,3 +7,6 @@ python manage.py migrate --noinput
 
 echo "Collecting static assets..."
 python manage.py collectstatic --noinput --ignore admin
+
+echo "Compiling static assets..."
+python manage.py compilestatic
