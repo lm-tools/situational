21a5a4aa216e259b890fc7682964bf22ec58c74f
camilleldn <camille@camillebaldock.co.uk>
2015-10-22 10:28:12 +0100

    Track SOC code selection per keyword

--- situational/assets/js/analytics/sectors.js
+++ situational/assets/js/analytics/sectors.js
@@ -5,3 +5,17 @@ function trackKeywordCount(num_keywords) {
 function trackKeyword(keyword) {
   ga('send', 'event', 'sectors', 'Keyword', keyword);
 }
+
+function trackSocCodeClicked(keyword, soc_code) {
+  ga(
+    'send',
+    'event',
+    'sectors',
+    'SocCodeClicked',
+    keyword,
+    1,
+    {
+      soc_code: soc_code
+    }
+  )
+}
